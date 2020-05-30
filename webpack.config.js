42dd929a2d7798a34938b172d960f548515c3b11
Sean Harte <scsharte@gmail.com>
2020-05-30 17:00:39 -0400

    add html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-    filename: 'main.js',
+    filename: 'main.[contentHash].js', // [contentHash] allows us to bust caching in the browser
     path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader', //3. injects styles into dom 
+                    'css-loader', //2. turns css into commonjs
+                    "sass-loader" //1. turns sass into css
+                ],
+            },
         ],
-      },
+    },
 };
