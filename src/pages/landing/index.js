3be63064cd89002ab06d20b28a0b9e751b8a8d61
Sean Harte <scsharte@gmail.com>
2020-05-31 15:00:49 -0400

    added normalize css and test js

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -4,3 +4,25 @@ import '../../index.scss';
 
 
 test('index');
+
+class Foo {
+    constructor(){
+        this.value = "foobar"
+    }
+    static instance() {
+        return new Foo()
+    }
+    getValue() {
+        return this.value
+    }
+}
+
+console.log(Foo.instance().getValue())
+
+async function apiCall() {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const data = await resp.json();
+    console.log(data);
+}
+
+apiCall();
