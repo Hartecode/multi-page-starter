e8e10c7df6b4a6cd67e1cb338c239d48a4982877
Sean Harte <scsharte@gmail.com>
2020-06-02 11:11:31 -0400

    added copy for storybook html

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -3,13 +3,19 @@ import { withActions } from '@storybook/addon-actions';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import '../../base.scss';
 import './button.scss';
+import copyCodeBlock from '@pickra/copy-code-block';
 
 
 storiesOf('Button', module)
     .addDecorator(withKnobs)
     .addDecorator(withActions('click', 'click .btn'))
     .add('button', () => {
-        return `<button class="btn" >${text('Text', 'Click Me')}</button>`;
+        return `
+            <button class="btn">${text('Text', 'Click Me')}</button>
+            <br>
+            <br>
+            ${copyCodeBlock(`<button class="btn">${text('Text', 'Click Me')}</button>`)}
+        `;
     })
     .add('test', () => {
         const button = document.createElement('button');
