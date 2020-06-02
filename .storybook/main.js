2ff793c459faf1bea57edcf347d593e9211fe6b9
Sean Harte <scsharte@gmail.com>
2020-06-02 18:37:44 -0400

    removed path from stoyrbook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 module.exports = {
   stories: [
-    '../stories/**/*.stories.js',
     '../src/components/**/*.stories.js',
     '../src/pages/**/*.stories.js'
   ],
